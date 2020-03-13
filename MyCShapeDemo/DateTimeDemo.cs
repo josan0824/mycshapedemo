e331b0f381e815ea03ea059f5f049b1c2353b6dc
tangjiangxu <2287280228@qq.com>
2020-03-13 14:08:39 +0800

    DateTime日期使用

---
+++ MyCShapeDemo/DateTimeDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCShapeDemo
+{
+    /// <summary>
+    /// DateTime测试
+    /// </summary>
+    class DateTimeDemo
+    {
+        static void Main(string[] args)
+        {
+            //1.得到当前时间
+            DateTime dateTime = DateTime.Now;
+            //2. 默认ToString方法格式：2020/03/13 13:35:20
+            Console.WriteLine($"{dateTime.ToString()}");
+            //3.利用ToString()方法可传入格式格式类型：y、M、d、h、m、s、f
+            Console.WriteLine($"{dateTime.ToString("yyyy-MM-dd hh:mm:ss:ff")}");
+            //4.还可根据对照表去生成
+            //https://www.cnblogs.com/sunbingqiang/p/10272814.html
+            Console.WriteLine($"{dateTime.ToString("U")}");
+
+            //5. 得到具体日期
+            int year = dateTime.Year;
+            int month = dateTime.Month;
+            int day = dateTime.Day;
+            string dayOfWeek = dateTime.DayOfWeek.ToString();
+            int hour = dateTime.Hour;
+            int minute = dateTime.Minute;
+            int second = dateTime.Second;
+
+            Console.WriteLine($"当前时间为：{year}年{month}月{day}日 {dayOfWeek} {hour}时{minute}分{second}秒");
+            Console.ReadLine();
+        }
+    }
+}
--- MyCShapeDemo/DictionaryDemo.cs
+++ MyCShapeDemo/DictionaryDemo.cs
@@ -12,106 +12,106 @@ namespace MyCShapeDemo
         //创建一个Dictionary
         static Dictionary<string, string> openWith = new Dictionary<string, string>();
         //main
-        static void Main(string[] args)
-        {
-            #region 1.插入
-            //使用Add方法向Dictionary中插入值
-            openWith.Add("txt", "notepad.exe");
-            openWith.Add("bmp", "paint.exe");
-            openWith.Add("dib", "paint.exe");
-            openWith.Add("rtf", "wordpad.exe");
+        //static void Main(string[] args)
+        //{
+        //    #region 1.插入
+        //    //使用Add方法向Dictionary中插入值
+        //    openWith.Add("txt", "notepad.exe");
+        //    openWith.Add("bmp", "paint.exe");
+        //    openWith.Add("dib", "paint.exe");
+        //    openWith.Add("rtf", "wordpad.exe");
 
-            try
-            {
-                openWith.Add("rtf", "wordpad2.exe");
-            }
-            catch (ArgumentException)
-            {
-                //如果插入相同的key，则会报ArgumentException错误，value相同是可以的
-                Console.WriteLine("An element with Key = \"txt\" already exists.");
-            }
+        //    try
+        //    {
+        //        openWith.Add("rtf", "wordpad2.exe");
+        //    }
+        //    catch (ArgumentException)
+        //    {
+        //        //如果插入相同的key，则会报ArgumentException错误，value相同是可以的
+        //        Console.WriteLine("An element with Key = \"txt\" already exists.");
+        //    }
 
-            //插入之前应该先用ContainsKey方法判断是否有值
-            if (!openWith.ContainsKey("containsKey"))
-            {
-                openWith.Add("containsKey", "containsKey.exe");
-                Console.WriteLine("Value added for key = \"containsKey\": {0}",
-                    openWith["containsKey"]);
-            }
-            #endregion
+        //    //插入之前应该先用ContainsKey方法判断是否有值
+        //    if (!openWith.ContainsKey("containsKey"))
+        //    {
+        //        openWith.Add("containsKey", "containsKey.exe");
+        //        Console.WriteLine("Value added for key = \"containsKey\": {0}",
+        //            openWith["containsKey"]);
+        //    }
+        //    #endregion
 
-            #region 2.读取
-            //通过key，使用索引直接读取value
-            Console.WriteLine("For key =\"rft\",value={0},", openWith["rtf"]);
+        //    #region 2.读取
+        //    //通过key，使用索引直接读取value
+        //    Console.WriteLine("For key =\"rft\",value={0},", openWith["rtf"]);
 
-            //如果key不存在，则会抛
-            try
-            {
-                Console.WriteLine("For key =\"notfoundkey\",value={0},", openWith["notfoundkey"]);
-            }
-            catch (KeyNotFoundException)
-            {
-                Console.WriteLine("Key = \"notfoundkey\" is not found.");
-            }
+        //    //如果key不存在，则会抛
+        //    try
+        //    {
+        //        Console.WriteLine("For key =\"notfoundkey\",value={0},", openWith["notfoundkey"]);
+        //    }
+        //    catch (KeyNotFoundException)
+        //    {
+        //        Console.WriteLine("Key = \"notfoundkey\" is not found.");
+        //    }
 
-            //使用TryGetValue把获取到的值，赋值给变量
-            string value = "";
-            if (openWith.TryGetValue("rtf", out value))
-            {
-                Console.WriteLine("For key = \"defaultKey\", value = {0}.", value);
-            }
-            else
-            {
-                Console.WriteLine("Key = \"defaultKey\" is not found.");
-            }
-
-
-            //使用键值对KeyValuePair来遍历
-            foreach (KeyValuePair<string, string> kvp in openWith)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}",kvp.Key, kvp.Value);
-            }
-
-            //使用Values属性得到单独的value
-            Dictionary<string, string>.ValueCollection valuecoll = openWith.Values;
-            foreach (string val in valuecoll)
-            {
-                Console.WriteLine("Value = {0}", val);
-            }
-
-            //使用Keys属性得到单独的keys
-            Dictionary<string, string>.KeyCollection keyColl = openWith.Keys;
-            foreach (string key in keyColl)
-            {
-                Console.WriteLine("Key = {0}", key);
-            }
-
-            #endregion
-
-            #region 3.修改
-            //通过key,使用索引直接给其赋值
-            openWith["rtf"] = "update wordpad.exe";
-            Console.WriteLine("For key =\"rft\",value={0},", openWith["rtf"]);
-
-            //如果key不存在，则会增加key，并赋值
-            openWith["addkey"] = "addkey.exe";
-            Console.WriteLine("For key =\"addkey\",value={0},", openWith["addkey"]);
-            #endregion
+        //    //使用TryGetValue把获取到的值，赋值给变量
+        //    string value = "";
+        //    if (openWith.TryGetValue("rtf", out value))
+        //    {
+        //        Console.WriteLine("For key = \"defaultKey\", value = {0}.", value);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Key = \"defaultKey\" is not found.");
+        //    }
 
 
-            #region 4.删除
-            //使用Remove方法，通过key移除
-            openWith.Add("removeKey", "removeValue");
-            Console.WriteLine("For key =\"removeKey\",value={0},", openWith["removeKey"]);
-            openWith.Remove("removeKey");
-            if (!openWith.ContainsKey("removeKey"))
-            {
-                Console.WriteLine("For key =\"removeKey\",is not found.");
-            }
+        //    //使用键值对KeyValuePair来遍历
+        //    foreach (KeyValuePair<string, string> kvp in openWith)
+        //    {
+        //        Console.WriteLine("Key = {0}, Value = {1}",kvp.Key, kvp.Value);
+        //    }
+
+        //    //使用Values属性得到单独的value
+        //    Dictionary<string, string>.ValueCollection valuecoll = openWith.Values;
+        //    foreach (string val in valuecoll)
+        //    {
+        //        Console.WriteLine("Value = {0}", val);
+        //    }
+
+        //    //使用Keys属性得到单独的keys
+        //    Dictionary<string, string>.KeyCollection keyColl = openWith.Keys;
+        //    foreach (string key in keyColl)
+        //    {
+        //        Console.WriteLine("Key = {0}", key);
+        //    }
+
+        //    #endregion
+
+        //    #region 3.修改
+        //    //通过key,使用索引直接给其赋值
+        //    openWith["rtf"] = "update wordpad.exe";
+        //    Console.WriteLine("For key =\"rft\",value={0},", openWith["rtf"]);
+
+        //    //如果key不存在，则会增加key，并赋值
+        //    openWith["addkey"] = "addkey.exe";
+        //    Console.WriteLine("For key =\"addkey\",value={0},", openWith["addkey"]);
+        //    #endregion
+
+
+        //    #region 4.删除
+        //    //使用Remove方法，通过key移除
+        //    openWith.Add("removeKey", "removeValue");
+        //    Console.WriteLine("For key =\"removeKey\",value={0},", openWith["removeKey"]);
+        //    openWith.Remove("removeKey");
+        //    if (!openWith.ContainsKey("removeKey"))
+        //    {
+        //        Console.WriteLine("For key =\"removeKey\",is not found.");
+        //    }
             
 
-            #endregion
-            Console.ReadLine();
-        }
+        //    #endregion
+        //    Console.ReadLine();
+        //}
     }
 }
